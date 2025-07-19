9133cd49344896f8da598fb5d7f3704fdce14e35
NaritaShosei <onigirikun7171@gmail.com>
2025-07-19 16:58:03 +0900

    update]コメントの生成と視聴者の管理クラス

--- Assets/Narita/CommentGenerator.cs
+++ Assets/Narita/CommentGenerator.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class CommentGenerator : MonoBehaviour
 {
-    
+    [SerializeField] Comment _commentData;
+    private string _comment;
+    private IViewer _viewer;
+
+    [ContextMenu("SetText")]
+    public void GetComment()
+    {
+        _comment = "かわいいね";
+        Comment com = Instantiate(_commentData,FindAnyObjectByType<Canvas>().transform);
+        com.SetText("OMAE", _comment);
+    }
+
+    public void SetInstance(IViewer viewer)
+    {
+        _viewer = viewer;
+    }
+
+    public void OnReply()
+    {
+        Debug.Log("返信されました");
+    }
+
 }
