52700f7fb74caba64c2d2250e1182f99f7b5c99c
sakamoto0508s12 <sakamoto0508s12@gmail.com>
2025-07-19 19:14:55 +0900

    [fix]スペルミス,intをfloat

--- Assets/Sakamoto/Viewer.cs
+++ Assets/Sakamoto/Viewer.cs
@@ -9,16 +9,16 @@ public class Viewer
 {
     public string Name { get; private set; }
     public bool IsSubscription { get; private set; }
-    public int SubscribTention { get; private set; }
+    public int SubscribTensions { get; private set; }
     public List<SuperChat> SuperChatList { get; private set; } = new List<SuperChat>();
 
-    private int _maxTime = 5, _minTime = 1;
-    private int _currentTention = 0;
+    private float _maxTime = 5, _minTime = 1;
+    private float _currentTension = 0;
 
     public Viewer(string name, (int SubscribTention, (int money, int tention)[] SuperChat) data)
     {
         Name = name;
-        SubscribTention = data.SubscribTention;
+        SubscribTensions = data.SubscribTention;
         foreach (var item in data.SuperChat)
         {
             SuperChatList.Add(new SuperChat(item.money, item.tention));
@@ -30,32 +30,32 @@ public class Viewer
     {
         while (true)
         {
-            int randTime = UnityEngine.Random.Range(_minTime, _maxTime);
-            await UniTask.Delay(randTime * 1000, cancellationToken: token);
+            float randTime = UnityEngine.Random.Range(_minTime, _maxTime);
+            await UniTask.Delay((int)(randTime * 1000), cancellationToken: token);
             //Debug.Log("コメント");
         }
     }
-    public void SetMaxTime(int max)
+    public void SetMaxTime(float max)
     {
         _maxTime = max;
     }
-    public void SetMinTime(int min)
+    public void SetMinTime(float min)
     {
         _minTime = min;
     }
-    public void TentionChange(int tention)
+    public void TensionChange(float tension)
     {
-        Debug.Log(_currentTention);
-        _currentTention += tention;
-        if (_currentTention >= SubscribTention && !IsSubscription)
+        Debug.Log(_currentTension);
+        _currentTension += tension;
+        if (_currentTension >= SubscribTensions && !IsSubscription)
         {
             IsSubscription = true;
-            SubscriptionManagement.Subscribers.Add(Name, (SubscribTention, SuperChatList.ToArray()));
+            SubscriptionManagement.Subscribers.Add(Name, (SubscribTensions, SuperChatList.ToArray()));
             Debug.Log("Add");
         }
-        foreach (var item in SuperChatList.OrderByDescending(a => a.Tention))
+        foreach (var item in SuperChatList.OrderByDescending(a => a.Tension))
         {
-            if (item.Tention <= _currentTention)
+            if (item.Tension <= _currentTension)
             {
                 //抽選コメントかスパチャか
                 int rand = Random.Range(0, 2);
@@ -64,7 +64,7 @@ public class Viewer
                 break;
             }
         }
-        if (_currentTention <= 0)
+        if (_currentTension <= 0)
         {
             IsSubscription = false;
             SubscriptionManagement.Subscribers.Remove(Name);
@@ -76,11 +76,11 @@ public class Viewer
 public class SuperChat
 {
     public int Money { get; private set; } = 1000;
-    public int Tention { get; private set; } = 10;
+    public int Tension { get; private set; } = 10;
 
-    public SuperChat(int money, int tention)
+    public SuperChat(int money, int tension)
     {
         Money = money;
-        Tention = tention;
+        Tension = tension;
     }
 }
