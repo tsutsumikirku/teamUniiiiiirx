c27f4c62d87ee10634710c2825f25523235f2ff9
NaritaShosei <onigirikun7171@gmail.com>
2025-07-19 14:39:57 +0900

    [add]クラスの作成など

---
+++ Assets/Narita/ViewerManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewerManager : MonoBehaviour
+{
+    List<IViewer> _viewerList = new();
+}
+
+public interface IViewer
+{
+
+}
