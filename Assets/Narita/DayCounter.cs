6066cbf10cf5085b78ccdb4f5dc3aa89385affd4
NaritaShosei <onigirikun7171@gmail.com>
2025-07-20 13:50:54 +0900

    [add]日数のデータ

---
+++ Assets/Narita/DayCounter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DayCounter : MonoBehaviour
+{
+    [SerializeField] private int _maxDayCount = 7;
+    private int _currentDay;
+
+    public bool HasReachedMaxDays => _currentDay >= _maxDayCount;
+
+    private void Awake()
+    {
+        ServiceLocater.Set(this);
+    }
+
+    public void AdvanceOneDay()
+    {
+        _currentDay++;
+    }
+}
